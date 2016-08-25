ef355e8e104d93cd50f6dbd77cf2fe084a261a70
Jacob Wood <jtwood@g.clemson.edu>
2016-08-25 14:37:17 -0400

    updated home
    added bootstrap
    reorganized some things

---
+++ www.JacobWood.com/scripts/oldscripts/RainbowTextSlidingV2.js
@@ -0,0 +1,39 @@
+﻿//some code from here - http://stackoverflow.com/questions/19165364/cycling-animation-of-rainbow-coloured-text
+//much modification
+$(document).ready(function () {
+  UpdateValues();
+});
+
+var itv;
+
+function UpdateValues() {
+  clearInterval(itv);
+  var step = document.getElementById('inputSpeed').value;
+  var lev = document.getElementById('inputLevel').value;
+  var ms = 30;  // loop every
+  var $uni = $('.unicorn');
+
+  if (isNaN(lev) || lev == 0) {
+    alert("Level must be a number not equal to 0");
+    return;
+  }
+
+  Array.from(document.getElementsByClassName("unicorn")).forEach(function (item) {
+
+    var newContent = "";
+    var text = item.textContent
+    lev = text.length / lev;
+    for (var j = 0; j < text.length; j++) {
+      newContent += "<span style='color:hsla(" + j * lev + ", 100%, 50%, 1)'>" + text.charAt(j) + "</span>";
+    }
+    item.innerHTML = newContent;
+  });
+
+  var $ch = $uni.find('span'); // character
+  itv = setInterval(function () {
+    $ch.each(function () {
+      h = +$(this).attr('style').split(',')[0].split('(')[1] - step % 360;
+      $(this).attr({ style: "color:hsla(" + h + ", 100%, 50%, 1)" });
+    });
+  }, ms);
+}
---
+++ www.JacobWood.com/scripts/oldscripts/oldPasswordScript.js
@@ -0,0 +1,57 @@
+﻿$(document).ready(function () {
+  readCookie();
+});
+
+function checkIt() {
+
+  var p = 1;
+  var t = $('.password').val();
+  var e = "rd";
+  var a = "pa";
+  var cb = "ss";
+  var c = "aa";
+  var d = "15";
+  var dc = "12";
+  var b = "wo";
+  var f = "23";
+  c = cb;
+  while (p < 3) {
+    if (!t)
+      history.go(-1);
+    var bb = a + c + b;
+    var aa = e + d + f;
+    if (t.toLowerCase() == bb + aa) {
+      var name = "passwordCookie";
+      var value = "1"
+      document.cookie = name + "=" + value;
+      window.location.href = 'home.html';
+      break;
+    }
+    p += 1;
+    
+    alert('Access Denied - Password Incorrect, Please Try Again.');
+  }
+  if (t.toLowerCase() != "password" & p == 3)
+    window.location.href = 'http://www.google.com';
+  return " ";
+}
+
+function createCookie() {
+  var name = "passwordCookie";
+  var value = "1"
+  
+  document.cookie = name + "=" + value;
+}
+
+function readCookie(name) {
+  var nameEQ = "passwordCookie" + "=";
+  var ca = document.cookie;  
+  if (!ca.includes("passwordCookie=1"))
+    window.location.href = 'index.html';
+}
+
+function destroyCookie() {
+  var name = "passwordCookie";
+  var value = "0"
+  document.cookie = name + "=" + value;
+}
